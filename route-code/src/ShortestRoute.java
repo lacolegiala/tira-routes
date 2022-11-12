9ceaf6af0d2613349466cf7466fbad8cab5c999a
Sofia Riekkola <sofiariekkola@Sofia-MBP-2.home>
2022-11-12 20:21:58 +0200

    Move comments

--- route-code/src/ShortestRoute.java
+++ route-code/src/ShortestRoute.java
@@ -1,11 +1,9 @@
-/**
- * This will be given two arguments that represent addresses.
- * It then calculates the shortest possible route between them.
- *
- * @param address1 the starting point.
- * @param address2 the destination.
- */
-
 public class ShortestRoute {
-
+    /**
+     * This will be given two arguments that represent addresses.
+     * It then calculates the shortest possible route between them.
+     *
+     * @param address1 the starting point.
+     * @param address2 the destination.
+     */
 }
